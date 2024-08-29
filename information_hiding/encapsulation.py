da65f862b4b17f5daa73f70733b75a5101b8bc5d
Rv607x <19752692+Rv607x@users.noreply.github.com>
2024-08-29 06:02:15 +0300

    example of information hiding using encapsulation

--- information_hiding/encapsulation.py
+++ information_hiding/encapsulation.py
@@ -19,7 +19,29 @@ class User:
             print("Invalid credentials")
 
 Steve = User("Steve", "12345")
-Steve.login("steve", "12345")
-Steve.login("steve", "6789")
-Steve.password= "6789"
-Steve.login("steve", "6789")
+Steve.login("steve", "12345")   # grants access, correct username and password
+Steve.login("steve", "6789")    # invalid credentials due to wrong password
+Steve.password= "6789"          
+Steve.login("steve", "6789")    # grants access due to edit of password property
+Steve.login("steeve", "6789")   # invalid crediatials due to wrong username
+
+
+# good example of encapsulation
+
+class User2:
+    def __init__(self, userName = None, password = None):
+        self.__userName = userName
+        self.__password = password
+
+    def login(self, userName, password):
+        if ((self.__userName.lower() == userName.lower())
+             and (self.__password == password)):
+            print("Access granted againist Username")
+        else:
+            print("Invalid Credentials")
+
+Kevo = User2("Kevo", "12345")
+Kevo.login("Kevo", "12345")
+
+Kevo.login("Kevo", "6789")
+Kevo.__password
